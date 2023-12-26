466c63ec0994a473af6faaf89fff1e5830131671
HarshanaWAJ <janithharshana2019@gmail.com>
2023-12-26 17:28:01 +0530

    Landing UI

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -6,124 +6,142 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 1.6,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 1.6,
-                  decoration: const BoxDecoration(
-                    color: Color(0xFFE8D9C9),
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(100.0),
+    return SingleChildScrollView(
+      child: Material(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 1.6,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
                     ),
                   ),
-                  child: Center(
-                    child: Image.asset(
-                      "lib/assets/logo.png",
-                      scale: 0.8,
-                    ),
-                  ),
-                ),
-                //Must Add APP NAME In padding
-              ],
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2.666,
-                decoration: const BoxDecoration(
-                  color: Color(0xFFE8D9C9),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2.666,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(80.0),
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    const Text(
-                      "Welcome To SMART Travellig!",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 2.0,
-                        wordSpacing: 5.0,
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 1.6,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFD5A09D),
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(100.0),
                       ),
                     ),
-                    const SizedBox(
-                      height: 15.0,
+                    child: Center(
+                      child: Image.asset(
+                        "lib/assets/bgpic.png",
+                        scale: 0.8,
+                      ),
                     ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40.0),
-                      child: Opacity(
-                        opacity: 0.6,
+                  ),
+                  //Must Add APP NAME In padding
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.666,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFD5A09D),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.666,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(80.0),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      const Text(
+                        "Welcome To SMART Travellig!",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 2.0,
+                          wordSpacing: 5.0,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 40.0),
+                        child: Opacity(
+                          opacity: 0.6,
+                          child: Text(
+                            "Make your journy more easier with others. Be SMART!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      Material(
+                        color: const Color(0xFFD5A09D),
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: InkWell(
+                          //Function
+                          onTap: () {
+                            debugPrint("Tapped on GET STARTED");
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 80.0,
+                            ),
+                            child: (const Text(
+                              "Get Start",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 2.0,
+                              ),
+                            )),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50.0,
+                      ),
+                      const Opacity(
+                        opacity: 0.4,
                         child: Text(
-                          "Make your journies more easir with SMART TRAVELLING.",
-                          textAlign: TextAlign.center,
+                          "Powered by JH Solutions",
                           style: TextStyle(
-                            fontSize: 16.0,
+                            fontSize: 14.0,
                             fontWeight: FontWeight.w500,
                             color: Colors.black,
                           ),
                         ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15.0,
-                    ),
-                    Material(
-                      color: Color(0xFFE8D9C9),
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: InkWell(
-                        //Function
-                        onTap: () {},
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 15,
-                            horizontal: 80.0,
-                          ),
-                          child: (const Text(
-                            "Get Start",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 2.0,
-                            ),
-                          )),
-                        ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
