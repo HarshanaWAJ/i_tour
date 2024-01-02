0bb86b0442370b508741922b1abeeea871f95a49
HarshanaWAJ <janithharshana2019@gmail.com>
2024-01-02 17:46:20 +0530

    user_auth

--- lib/pages/user_auth.dart
+++ lib/pages/user_auth.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:i_tour/pages/profile.dart';
+import 'package:i_tour/pages/login.dart'; // Assuming you have a login page
 
 class UserAuth extends StatelessWidget {
   const UserAuth({super.key});
@@ -12,11 +13,15 @@ class UserAuth extends StatelessWidget {
         body: StreamBuilder<User?>(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
-            //User Login
+            if (snapshot.hasError) {
+              return const Center(
+                  child: Text("Authentication Error")); // Handle errors
+            }
+
             if (snapshot.hasData) {
-              return const MyProfile();
+              return const MyProfile(); // Show profile if user is authenticated
             } else {
-              return const MyProfile();
+              return LoginPage(); // Show login page if not authenticated
             }
           },
         ),
